d1fe7775349d1ed01293a5fe45d64652bd4d4fb1
Alan <adsonnheim@gmail.com>
2026-02-02 10:29:29 -0600

    Added addition and subtraction functionality, with basic input checking.

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <limits>
 using namespace std;
+
+bool isOperator(string input) {
+    if (input == "+" || input == "-") {
+        return true;
+    }
+    return false;
+}
 
 int main() {
     cout << "Hello, and welcome to my calculator program!" << endl;
@@ -8,15 +16,28 @@ int main() {
     cout << "To exit the program at any time, enter 'exit'." << endl;
     
     float value = 0.0;
-
-    cout << "Current value : " << value << endl;
     
     string input;
-    cin >> input;
+    double nextNum;
     
     while (input != "exit") {
+        cout << "To exit the program at any time, enter 'exit'." << endl;
+        cout << "Please enter a number or an operator." << endl;
         cout << "Current value : " << value << endl;
         cin >> input;
+        if (isOperator(input)) {
+            cout << value << " " << input << " ";
+            while (!(cin >> nextNum)) {
+                cout << "Invalid input!" << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<int>::max(), '\n');
+            }
+            if (input == "+") { 
+                value = value + nextNum;
+            } else if (input == "-") {
+                value = value - nextNum;
+            }
+        }
     }
 
     return 0;
